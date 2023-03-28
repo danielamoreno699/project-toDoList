29e8b5293419f1c222694a5e38847e16e4b556fd
Daniela Moreno <danielamoreno699@gmail.com>
2023-03-28 15:12:48 -0500

    Install webpack depedencies

---
+++ src/index.js
@@ -0,0 +1,36 @@
+//import _ from 'lodash';
+import './style.css';
+
+// function component() {
+//   const element = document.createElement('div');
+
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//   element.classList.add('hello');
+
+//   return element;
+// }
+// document.body.appendChild(component());
+
+const arrayToDoList = [
+  {
+    desc: 'finish microverse proyect',
+    completed: false,
+    index: 1,
+  },
+  {
+    desc: 'buy groceries',
+    completed: false,
+    index: 2,
+  },
+  {
+    desc: 'appointment with friend',
+    completed: false,
+    index: 3,
+  },
+
+];
+
+// arrayToDoList.forEach((toDo)=> {
+//     const ul = document.getElementById('ul-todo');
+
+// })
