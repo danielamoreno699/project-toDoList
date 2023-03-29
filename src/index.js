4d68b486fee6a424768032c7166b6e2b373c3125
Daniela Moreno <danielamoreno699@gmail.com>
2023-03-29 16:19:00 -0500

    Add js classes for removal and clear local storage

--- src/index.js
+++ src/index.js
@@ -14,20 +14,21 @@ class UI {
     this.updateCheck = this.updateCheck.bind(this);
   }
 
-  displayToDo = (todo) => {
+  displayToDo = (todo, index) => {
     const ul = document.getElementById('items');
     const li = document.createElement('li');
     const hr = document.createElement('hr');
     li.classList.add('flex-between');
 
     li.innerHTML = `<div class="container-input">
-    <input id="${todo.index}" type="checkbox" class="checkbox">
-    <label for="${todo.index}" class="todo-list-item ${todo.completed === true ? 'completed' : ''}">
+    <input id="${index}" type="checkbox" class="checkbox">
+    <label for="${index}" class="todo-list-item ${todo.completed === true ? 'completed' : ''}">
     ${todo.desc}</label>
     
             
     </div>
-    <i class="fa-solid fa-trash-can delete" id="delete-task"></i>`;
+    <button class="delete"><i class="fa-solid fa-trash-can" id="delete-task"></i></button>
+    `;
 
     ul.appendChild(li);
     ul.appendChild(hr);
@@ -71,9 +72,9 @@ class Store {
 
   static displaytoDolist() {
     const todo = Store.getToDoList();
-    todo.forEach((todoItem) => {
+    todo.forEach((todoItem, index) => {
       const ui = new UI();
-      ui.displayToDo(todoItem);
+      ui.displayToDo(todoItem, index);
     });
   }
 
@@ -84,63 +85,49 @@ class Store {
   }
 
   static removeList(target) {
-    if (target.className === 'delete') {
-      const itemRemoved = parseInt(target.previousElementSibling.htmlFor, 10);
-      const todoList = Store.getToDoList();
-      const findIndex = todoList.findIndex((todo) => todo.index === itemRemoved);
-      todoList.splice(findIndex, 1);
-      localStorage.setItem('todoList', JSON.stringify(todoList));
-      target.parentElement.remove();
-    }
+    const li = target.parentElement.parentElement;
+    const hr = li.nextElementSibling;
+    const itemRemoved = parseInt(li.querySelector('label').htmlFor, 10);
+    const todoList = Store.getToDoList();
+    const findIndex = todoList.findIndex((index) => index === itemRemoved);
+    todoList.splice(findIndex, 1);
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+    hr.remove();
+    li.remove();
   }
 }
 
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
-    const ui = new UI();
-   
-   
-    const inputList = document.getElementById('myInput');
-  
-    
-  
-    inputList.addEventListener('keypress', (event) => {
-      const list = inputList.value;
-    
-      if (!list) {
-        return null;
-      }
-    
-      if (event.key === 'Enter') {
-        const newToDo = new ToDoList(list);
-        ui.displayToDo(newToDo);
-        Store.addList(newToDo);
-        ui.clearFieldInput();
-        ui.registerEventListeners();
-        event.preventDefault();
-      }
-    });
-  
-    Store.displaytoDolist();
-    
+  const ui = new UI();
+  const inputList = document.getElementById('myInput');
+  inputList.addEventListener('keypress', (event) => {
+    const list = inputList.value;
+    if (!list) {
+      return null;
+    }
+
+    if (event.key === 'Enter') {
+      const newToDo = new ToDoList(list);
+      ui.displayToDo(newToDo, Store.getToDoList().length);
+      Store.addList(newToDo);
+      ui.clearFieldInput();
+      ui.registerEventListeners();
+      event.preventDefault();
+    }
+    return null;
   });
- 
-//   const updateCheck = (e) => {
-//     if (e.target.checked === true) {
-//       e.target.nextElementSibling.classList.add('completed');
-//     }
-  
-//     if (!e.target.checked) {
-//       e.target.nextElementSibling.classList.remove('completed');
-//     }
-//   };
-  
-//   const checkboxes = document.querySelectorAll('.checkbox');
-//   checkboxes.forEach((checkbox) => {
-//     checkbox.addEventListener('click', updateCheck);
-//   });
-  
- 
-  
+
+  Store.displaytoDolist();
+});
+
+document.getElementById('items').addEventListener('click', (e) => {
+  Store.removeList(e.target);
+  e.preventDefault();
+});
+
+document.getElementById('btn-clearAll').addEventListener('click', () => {
+  if (Store.getToDoList().length >= 1) {
+    localStorage.clear();
+    window.location.reload();
+  }
+});
