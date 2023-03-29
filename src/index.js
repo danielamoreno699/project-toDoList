850f46731d01aaafb5b98fef55ed3cc26c6b49a5
Daniela Moreno <danielamoreno699@gmail.com>
2023-03-28 23:01:50 -0500

    Add styles css and some functions

--- src/index.js
+++ src/index.js
@@ -23,16 +23,35 @@ const displayLi = () => {
   arrayToDoList.forEach((todo) => {
     const ul = document.getElementById('items');
     const li = document.createElement('li');
+    const hr = document.createElement('hr');
     li.classList.add('flex-between');
 
     li.innerHTML = `<div class="container-input">
-    <input id="${todo.id}" type="checkbox" class="checkbox"checked>
-    <label for="item1">${todo.desc}</label>
+    <input id="${todo.index}" type="checkbox" class="checkbox">
+    <label for="${todo.index}" class="todo-list-item ${todo.completed === true ? 'completed' : ''}">
+    ${todo.desc}</label>
+    
             
     </div>
     <i class="fa-solid fa-trash-can" id="delete-task"></i>`;
 
     ul.appendChild(li);
+    ul.appendChild(hr);
   });
 };
 displayLi();
+
+const updateCheck = (e) => {
+  if (e.target.checked === true) {
+    e.target.nextElementSibling.classList.add('completed');
+  }
+
+  if (!e.target.checked) {
+    e.target.nextElementSibling.classList.remove('completed');
+  }
+};
+
+const checkboxes = document.querySelectorAll('.checkbox');
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener('click', updateCheck);
+});
