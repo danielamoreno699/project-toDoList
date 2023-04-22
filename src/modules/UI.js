081fe9ce656803bc6f572c4fbc70d9570e349445
Daniela Moreno <dani9292@hotmail.com>
2023-04-22 09:52:17 -0500

    Update

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,38 +1,43 @@
 class UI {
   constructor() {
     this.updateCheck = this.updateCheck.bind(this);
+    this.dragEnter = this.dragEnter.bind(this);
+    this.dragStartIndex = null;
   }
 
     // displays array of lists
     displayToDo = (todo) => {
       const ul = document.getElementById('items');
       const li = document.createElement('li');
-      const hr = document.createElement('hr');
+      // const hr = document.createElement('hr');
       li.classList.add('flex-between');
 
-      li.innerHTML = `<div class="container-input  draggable" draggable="true"  >
-        <input id="${todo.index}" type="checkbox" class="checkbox" ${todo.completed ? 'checked' : ''}>
+      li.innerHTML = `
+
+      <div class= "draggable-list draggable"  draggable="true" data-index="${todo.index}">
+      <div class=  "container-input "  >
+      
+ 
+      <input id="${todo.index}" type="checkbox" class="checkbox" ${todo.completed ? 'checked' : ''}>
         <input id="${todo.index}-inputTxt" class="todo-list-item ${todo.completed ? 'completed' : ''}" type="text" value="${todo.desc}">
       </div>
-      <button class="delete" data-index="${todo.index}"><i class="fa-solid fa-trash-can" id="delete-task"></i></button>`;
+      <button class="delete" data-index="${todo.index}"><i class="fa-solid fa-trash-can" id="delete-task"></i></button> </div>`;
 
       ul.appendChild(li);
-      ul.appendChild(hr);
+      // ul.appendChild(hr);
 
       this.registerEventListeners();
     };
 
-    // clear inputs
     clearFieldInput =() => {
       const inputList = document.getElementById('myInput');
       inputList.value = '';
     }
 
-    // update checkbox when selected
     updateCheck = (e) => {
-      const todoList = JSON.parse(localStorage.getItem('todoList')) || [];
       const index = e.target.id;
       const input = document.querySelector(`input[id="${index}-inputTxt"].todo-list-item`);
+      const todoList = JSON.parse(localStorage.getItem('todoList')) || [];
       const todoItem = todoList.find((item) => item.index === Number(index));
 
       if (todoItem) {
@@ -52,12 +57,85 @@ class UI {
       updateItem = (e) => {
         const index = e.target.id.split('-')[0];
         const { value } = e.target;
-        const todoList = JSON.parse(localStorage.getItem('todoList')) || []
+        const todoList = JSON.parse(localStorage.getItem('todoList')) || [];
+
         const todoItem = todoList.find((item) => item.index === Number(index));
         todoItem.desc = value;
-        todoItem.completed = e.target.classList.contains('completed');
         localStorage.setItem('todoList', JSON.stringify(todoList));
       };
+
+      // dragStar function
+      dragStart = (e) => {
+        const div = e.target.closest('div');
+        const dragStartIndex = div.dataset.index;
+        console.log('event', 'dragstart');
+        console.log('indexStart', dragStartIndex);
+        this.dragStartIndex = dragStartIndex;
+      }
+
+       // dragEnter function
+       dragEnter = (e) => {
+         const li = e.target.parentNode;
+         li.classList.add('over');
+       }
+
+       // dragStar function
+       dragLeave = (e) => {
+         const li = e.target.parentNode;
+         li.classList.remove('over');
+       }
+
+       // dragStar function
+       dragOver = (e) => {
+         e.preventDefault();
+       }
+
+       // dragStar function
+       dragDrop = (e) => {
+         const draggableEl = e.target.closest('.draggable');
+         const dragEndIndex = draggableEl.getAttribute('data-index');
+
+         const li = e.target.parentNode;
+
+         console.log('indexdrop', dragEndIndex);
+         this.swapItems(this.dragStartIndex, dragEndIndex);
+         li.classList.remove('over');
+         console.log('event', 'dragdrop');
+       }
+
+       // SawpItems function
+
+      swapItems = (fromIndex, toIndex) => {
+        const todoList = JSON.parse(localStorage.getItem('todoList')) || [];
+
+        console.log('fromIndex:', fromIndex);
+        console.log('toIndex:', toIndex);
+        console.log('todoList:', todoList);
+
+        const elementOne = document.querySelector(`[data-index="${fromIndex}"]`);
+        const elementTwo = document.querySelector(`[data-index="${toIndex}"]`);
+
+        console.log('ele1', elementOne);
+        console.log('ele2', elementTwo);
+
+        const parentOne = elementOne.parentElement;
+        const parentTwo = elementTwo.parentElement;
+
+        parentOne.appendChild(elementTwo);
+        parentTwo.appendChild(elementOne);
+
+        // update the todoList array with the new order of items
+        const indexOne = todoList.findIndex((item) => item.index === fromIndex);
+        const indexTwo = todoList.findIndex((item) => item.index === toIndex);
+
+        const temp = todoList[indexOne];
+        todoList[indexOne] = todoList[indexTwo];
+        todoList[indexTwo] = temp;
+
+        localStorage.setItem('todoList', JSON.stringify(todoList));
+
+        // Store the updated list in local storage
+      }
 
       // event registration for checkbox and input text. Updates values
       registerEventListeners = () => {
@@ -69,6 +147,20 @@ class UI {
         const inputTexts = document.querySelectorAll('.todo-list-item');
         inputTexts.forEach((inputText) => {
           inputText.addEventListener('input', this.updateItem);
+        });
+
+        const draggables = document.querySelectorAll('li');
+        const dragListItems = document.querySelectorAll('li');
+
+        draggables.forEach((draggable) => {
+          draggable.addEventListener('dragstart', this.dragStart);
+        });
+
+        dragListItems.forEach((item) => {
+          item.addEventListener('dragover', this.dragOver);
+          item.addEventListener('drop', this.dragDrop);
+          item.addEventListener('dragenter', this.dragEnter);
+          item.addEventListener('dragleave', this.dragLeave);
         });
       };
 }
